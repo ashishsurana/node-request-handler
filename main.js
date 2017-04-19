219e43f7c97d41fae2e36e096b986babde1c3522
Ashish Surana <ashishsurana12345@gmail.com>
2017-04-19 19:14:07 +0530

    API1 completed

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
-// var mongoose = require('mongoose');
+// var mongoose = require('mong oose');
 
-app.get('/test',function (req, res , next){
-    res.send("Hello World");
-})
+
+
+app.get('/api/request/', function (req, res, next) {
+    let args = req.query;
+
+    if (args.connId == undefined || args.timeout == undefined) {
+        console.log("Bad request");
+        res.status(500).end();
+    } else {
+        setTimeout(function () {
+            res.send({ "status": "ok" });
+        }, args.timeout);
+    }
+});
 
 app.listen(3000, function () {
-	console.log('Example app running on  3000')
+    console.log('Example app running on  3000')
 });
